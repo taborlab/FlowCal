d8f935c2d476c85a9b39a1cf2de7004f67e3961a
John T. Sexton <john.t.sexton@rice.edu>
2015-02-02 14:01:58 -0600

    Improved fc.plot.fsc_ssc
    Plotted 2D histogram instead of scatter plot. Added some plotting options.

--- fc/plot.py
+++ fc/plot.py
@@ -6,17 +6,45 @@
 # Date: 2/2/2015
 #
 # Requires:
+#   * numpy
 #   * matplotlib
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-def fsc_ssc(data):
+def fsc_ssc(data,
+            axes_limits=[0,1023,0,1023],
+            title=None,
+            colorbar=True,
+            ax=None):
     '''Plot FSC v SSC.
 
-    data - NxD numpy array (row=event), 1st column=FSC, 2nd column=SSC'''
+    data        - NxD numpy array (row=event), 1st column=FSC, 2nd column=SSC
+    axes_limits - axis boundaries (default=[0,1023,0,1023])
+    title       - string to label plot (default=None)
+    colorbar    - show colorbar (default=True)
+    ax          - matplotlib axis object (default=None)'''
     
-    plt.scatter(data[:,0], data[:,1], marker='.')
-    plt.axis([0,1023,0,1023])
-    plt.xlabel('FSC')
-    plt.ylabel('SSC')
+    # make 2D histogram of FSC v SSC
+    e = np.arange(1025)-0.5      # bin edges (centered over 0 - 1023)
+    H,xe,ye = np.histogram2d(data[:,0], data[:,1], bins=e)
+    H = H.T     # numpy transposes axes to be consistent with histogramnd
+
+    # plot results
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+
+    img = ax.imshow(H,origin='lower')
+
+    if colorbar:
+        plt.colorbar(img, ax=ax)
+
+    ax.axis(axes_limits)
+    ax.set_xlabel('FSC')
+    ax.set_ylabel('SSC')
+
+    if not (title is None):
+        ax.set_title(str(title))
+
     plt.show()
