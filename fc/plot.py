9ee7847a99ffe1d56bbadb5d01666caef5e70576
John T. Sexton <john.t.sexton@rice.edu>
2015-02-02 00:53:30 -0600

    Simply fc.plot.fsc_ssc implementation

--- fc/plot.py
+++ fc/plot.py
@@ -1,2 +1,22 @@
+#!/usr/bin/python
+#
+# plot.py - Module containing plotting functions for flow cytometry data sets.
+#
+# Author: John T. Sexton (john.t.sexton@rice.edu)
+# Date: 2/2/2015
+#
+# Requires:
+#   * matplotlib
+
+import matplotlib.pyplot as plt
+
 def fsc_ssc(data):
-    pass
+    '''Plot FSC v SSC.
+
+    data - NxD numpy array (row=event), 1st column=FSC, 2nd column=SSC'''
+    
+    plt.scatter(data[:,0], data[:,1], marker='.')
+    plt.axis([0,1023,0,1023])
+    plt.xlabel('FSC')
+    plt.ylabel('SSC')
+    plt.show()
