c62f9637007a53d348a26bc94f5c7d5821c8ac09
Sebastian M. Castillo Hair <castillohair@gmail.com>
2015-07-08 01:58:27 -0500

    Added to_mef function in transform.py.

--- fc/transform.py
+++ fc/transform.py
@@ -52,7 +52,7 @@ def transform(data, channels, transform_fxn, def_channels = None):
     # Default
     if channels is None:
         if def_channels is None:
-            channels = range(data.shape[1])
+            channels = range(data_t.shape[1])
         else:
             channels = def_channels
     # Convert channels to iterable
@@ -62,17 +62,16 @@ def transform(data, channels, transform_fxn, def_channels = None):
     data_t[:,channels] = transform_fxn(data_t[:,channels])
     # Apply transformation to range
     if hasattr(data_t, 'channel_info'):
-        if hasattr(channels, '__iter__'):
-            for channel in channels:
-                if isinstance(channel, basestring):
-                    ch = data_t.name_to_index(channel)
-                else:
-                    ch = channel
-                if 'range' in data_t.channel_info[ch]:
-                    r = data_t.channel_info[ch]['range']
-                    r[0] = transform_fxn(r[0])
-                    r[1] = transform_fxn(r[1])
-                    data_t.channel_info[ch]['range'] = r
+        for channel in channels:
+            if isinstance(channel, basestring):
+                ch = data_t.name_to_index(channel)
+            else:
+                ch = channel
+            if 'range' in data_t.channel_info[ch]:
+                r = data_t.channel_info[ch]['range']
+                r[0] = transform_fxn(r[0])
+                r[1] = transform_fxn(r[1])
+                data_t.channel_info[ch]['range'] = r
 
     return data_t
 
@@ -90,8 +89,81 @@ def exponentiate(data, channels = None):
 
     returns  - NxD FCSData object or numpy array. '''
 
+    # Transform and return
     def transform_fxn(x): return 10**(x/256.0)
     return transform(data, channels, transform_fxn)
+
+
+def to_mef(data, channels, sc_list, sc_channels = None):
+    '''Transform data to MEF units using standard curves obtained from 
+    calibration beads.
+
+    This function allows for the specification of the channels in which the
+    MEF transformation should be applied. The function automatically checks 
+    whether a standard curve is available for the specified channel, and throws
+    and error otherwise.
+
+    It is recommended that after calculating the standard curves, this function
+    be reduced using functools.partial to the following signature:
+        to_mef(data, channels)
+
+    Arguments:
+    data        - NxD FCSData object or numpy array
+    channels    - Channels in which to perform the transformation. If channels 
+                    is None, use all channels in sc_channels
+    sc_list     - List of functions implementing the standard curves for each
+                    of the channels in sc_channels.
+    sc_channels - List of channels in which the standard curves are available. 
+                    If None, use all channels in data.
+    '''
+
+    # Default sc_channels
+    if sc_channels is None:
+        if data.ndim == 1:
+            sc_channels = range(data.shape[0])
+        else:
+            sc_channels = range(data.shape[1])
+    # Check that sc_channels and sc_list have the same length
+    assert len(sc_channels) == len(sc_list), \
+        "sc_channels and sc_list should have the same length."
+    # Convert sc_channels to index
+    if isinstance(sc_channels[0], basestring):
+        sc_channels = data.name_to_index(sc_channels)
+
+    # Default channels
+    if channels is None:
+        channels = sc_channels
+    # Convert channels to iterable
+    if not hasattr(channels, '__iter__'):
+        channels = [channels]
+    # Convert channels to index
+    if isinstance(channels[0], basestring):
+        channels_ind = data.name_to_index(channels)
+    else:
+        channels_ind = channels
+    # Check if every channel is in sc_channels
+    for chi, chs in zip(channels_ind, channels):
+        if chi not in sc_channels:
+            raise ValueError("No standard curve for channel {}.".format(chs))
+
+    # Copy data array
+    data_t = data.copy().astype(np.float64)
+
+    # Iterate over channels
+    for chi, sc in zip(sc_channels, sc_list):
+        if chi not in channels_ind:
+            continue
+        # Apply transformation
+        data_t[:,chi] = sc(data_t[:,chi])
+        # Apply transformation to range
+        if hasattr(data_t, 'channel_info'):
+            if 'range' in data_t.channel_info[chi]:
+                r = data_t.channel_info[chi]['range']
+                r[0] = sc(r[0])
+                r[1] = sc(r[1])
+                data_t.channel_info[chi]['range'] = r
+
+    return data_t
 
 
 def _clustering_dbscan(data, eps = 20.0, min_samples = 40):
--- test/test_transform.py
+++ test/test_transform.py
@@ -129,5 +129,134 @@ class TestExponentiateFCS(unittest.TestCase):
         numpy.testing.assert_array_equal(dt[:,4], 10**(self.d[:,4]/256.0))
         numpy.testing.assert_array_equal(dt[:,5], self.d[:,5])
 
+class TestMefArray(unittest.TestCase):
+    def setUp(self):
+        self.d = numpy.array([
+            [1, 7, 2],
+            [2, 8, 3],
+            [3, 9, 4],
+            [4, 10, 5],
+            [5, 1, 6],
+            [6, 2, 7],
+            [7, 3, 8],
+            [8, 4, 9],
+            [9, 5, 10],
+            [10, 6, 1],
+            ])
+        self.sc0 = lambda x: x + 10
+        self.sc1 = lambda x: x**2
+        self.sc2 = lambda x: numpy.log(x)
+
+    def test_mef_length_error(self):
+        self.assertRaises(AssertionError, fc.transform.to_mef, 
+                            self.d, 1, [self.sc1], [1,2])
+
+    def test_mef_channel_error(self):
+        self.assertRaises(ValueError, fc.transform.to_mef, 
+                            self.d, 0, [self.sc1, self.sc2], [1,2])
+
+    def test_mef_1d_1(self):
+        dt = fc.transform.to_mef(self.d, 1, [self.sc1, self.sc2], [1,2])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1]**2.)
+        numpy.testing.assert_array_equal(dt[:,2], self.d[:,2])
+
+    def test_mef_1d_2(self):
+        dt = fc.transform.to_mef(self.d, 2, [self.sc1, self.sc2], [1,2])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1])
+        numpy.testing.assert_array_equal(dt[:,2], numpy.log(self.d[:,2]))
+
+    def test_mef_2d(self):
+        dt = fc.transform.to_mef(self.d, [1,2], [self.sc1, self.sc2], [1,2])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1]**2.)
+        numpy.testing.assert_array_equal(dt[:,2], numpy.log(self.d[:,2]))
+
+    def test_mef_default_channel(self):
+        dt = fc.transform.to_mef(self.d, None, [self.sc1, self.sc2], [1,2])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1]**2.)
+        numpy.testing.assert_array_equal(dt[:,2], numpy.log(self.d[:,2]))
+
+    def test_mef_default_sc_channel(self):
+        dt = fc.transform.to_mef(self.d, None, 
+            [self.sc0, self.sc1, self.sc2], None)
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0] + 10)
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1]**2.)
+        numpy.testing.assert_array_equal(dt[:,2], numpy.log(self.d[:,2]))
+
+    def test_mef_default_sc_channel_error(self):
+        self.assertRaises(AssertionError, fc.transform.to_mef, 
+                            self.d, None, [self.sc1, self.sc2], None)
+
+class TestMefFCS(unittest.TestCase):
+    def setUp(self):
+        self.channel_names = ['FSC-H', 'SSC-H', 'FL1-H', 
+                                'FL2-H', 'FL3-H', 'Time']
+        self.d = fc.io.TaborLabFCSData('test/Data.001')
+        self.n_samples = self.d.shape[0]
+        self.sc0 = lambda x: x + 10
+        self.sc1 = lambda x: x**2
+        self.sc2 = lambda x: numpy.log(x + 1)
+
+    def test_mef_length_error(self):
+        self.assertRaises(AssertionError, fc.transform.to_mef, 
+                    self.d, 'FL1-H', [self.sc1], ['FL1-H','FL3-H'])
+
+    def test_mef_channel_error(self):
+        self.assertRaises(ValueError, fc.transform.to_mef, 
+                    self.d, 'FSC-H', [self.sc1, self.sc2], ['FL1-H','FL3-H'])
+
+    def test_mef_1d_1(self):
+        dt = fc.transform.to_mef(self.d, 'FL1-H', 
+            [self.sc1, self.sc2], ['FL1-H','FL3-H'])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1])
+        numpy.testing.assert_array_equal(dt[:,2], self.d[:,2]**2.)
+        numpy.testing.assert_array_equal(dt[:,3], self.d[:,3])
+        numpy.testing.assert_array_equal(dt[:,4], self.d[:,4])
+
+    def test_mef_1d_2(self):
+        dt = fc.transform.to_mef(self.d, 'FL3-H', 
+            [self.sc1, self.sc2], ['FL1-H','FL3-H'])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1])
+        numpy.testing.assert_array_equal(dt[:,2], self.d[:,2])
+        numpy.testing.assert_array_equal(dt[:,3], self.d[:,3])
+        numpy.testing.assert_array_equal(dt[:,4], 
+            numpy.log(self.d[:,4].astype(numpy.float64) + 1))
+
+    def test_mef_2d(self):
+        dt = fc.transform.to_mef(self.d, ['FL1-H','FL3-H'], 
+            [self.sc1, self.sc2], ['FL1-H','FL3-H'])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1])
+        numpy.testing.assert_array_equal(dt[:,2], self.d[:,2]**2.)
+        numpy.testing.assert_array_equal(dt[:,3], self.d[:,3])
+        numpy.testing.assert_array_equal(dt[:,4], 
+            numpy.log(self.d[:,4].astype(numpy.float64) + 1))
+
+    def test_mef_default_channel(self):
+        dt = fc.transform.to_mef(self.d, None, 
+            [self.sc1, self.sc2], ['FL1-H','FL3-H'])
+        numpy.testing.assert_array_equal(dt[:,0], self.d[:,0])
+        numpy.testing.assert_array_equal(dt[:,1], self.d[:,1])
+        numpy.testing.assert_array_equal(dt[:,2], self.d[:,2]**2.)
+        numpy.testing.assert_array_equal(dt[:,3], self.d[:,3])
+        numpy.testing.assert_array_equal(dt[:,4], 
+            numpy.log(self.d[:,4].astype(numpy.float64) + 1))
+
+    def test_mef_range_channels(self):
+        dt = fc.transform.to_mef(self.d, ['FL1-H','FL3-H'], 
+            [self.sc1, self.sc2], ['FL1-H','FL3-H'])
+        cit = [dt.channel_info[i]['range'] for i in range(5)]
+        co = [[0, 1023, 1024],
+              [0, 1023, 1024],
+              [0**2, 1023**2, 1024],
+              [0, 1023, 1024],
+              [numpy.log(0 + 1), numpy.log(1023 + 1), 1024]]
+        self.assertEqual(cit, co)
+
 if __name__ == '__main__':
     unittest.main()
