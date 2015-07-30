90481d13621b8644f38835f3e9703bde0ff11649
Sebastian M. Castillo Hair <castillohair@gmail.com>
2015-07-29 20:32:04 -0500

    Deleted DESCRIPTION.rst.

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        '.': ['DESCRIPTION.rst'],
+        '.': ['CONTRIBUTE.rst'],
         'fc': ['diverging.csv', 'spectral.csv'],
     },
 
