f2c6e344978b0f971f30b6344705dba5fdcb3568
Sebastian Castillo-Hair <castillohair@gmail.com>
2015-08-17 10:11:44 -0500

    Modified run_no_excel.py to use fcs3.

--- examples/run_no_excel.py
+++ examples/run_no_excel.py
@@ -22,11 +22,25 @@ gated_plot_dir = 'plot_gated'
 plot_gated = True
 
 # Files
-beads_file = 'data.006'
-data_files = ['data.{:03d}'.format(i) for i in range(1,6)]
+beads_file = 'data_006.fcs'
+data_files = ['data_{:03d}.fcs'.format(i) for i in range(1,6)]
 
-# MEF values
-mefl = [numpy.nan, 792, 2079, 6588, 16471, 47497, 137049, 271647]
+# Channels
+sc_channels = ['FSC', 'SSC']
+fl_channels = ['FL1', 'FL2', 'FL3']
+# Colors for histograms
+cm = fc.plot.load_colormap('spectral', 3)
+hist_colors = dict(zip(fl_channels, cm[::-1]))
+
+# MEF channels
+mef_channels = ['FL1']
+# MEF type used per channel
+mef_names = {'FL1': 'Molecules of Equivalent Fluorescein, MEFL',
+            }
+# MEF bead values
+mef_values = {'FL1': [numpy.nan, 792, 2079, 6588, 16471, 
+                                                    47497, 137049, 271647],
+             }
 
 if __name__ == "__main__":
     # Check that directories exists, create if it doesn't.
@@ -41,16 +55,17 @@ if __name__ == "__main__":
     print "Beads file contains {} events.".format(beads_data.shape[0])
     # Trim
     beads_data = fc.gate.start_end(beads_data, num_start=250, num_end=100)
-    beads_data = fc.gate.high_low(beads_data, ['FSC-H', 'SSC-H'])
+    beads_data = fc.gate.high_low(beads_data, sc_channels)
     # Density gate
     print "\nRunning density gate on beads data..."
     gated_beads_data, gate_contour = fc.gate.density2d(data = beads_data,
-                                        gate_fraction = 0.3)
+                                                    channels = sc_channels,
+                                                    gate_fraction = 0.3)
     # Plot
     pyplot.figure(figsize = (6,4))
     fc.plot.density_and_hist(beads_data, gated_beads_data, 
-        density_channels = ['FSC-H', 'SSC-H'], 
-        hist_channels = ['FL1-H', 'FL2-H', 'FL3-H'],
+        density_channels = sc_channels,
+        hist_channels = fl_channels,
         gate_contour = gate_contour, 
         density_params = {'mode': 'scatter'}, 
         hist_params = {'ylim': (0, 1000), 'div': 4},
@@ -60,11 +75,11 @@ if __name__ == "__main__":
 
     # Obtain standard curve transformation
     print "\nCalculating standard curve..."
-    peaks_mef = numpy.array(mefl)
+    peaks_mef = numpy.array([mef_values[chi] for chi in mef_channels])
     to_mef = fc.mef.get_transform_fxn(gated_beads_data, peaks_mef, 
                     cluster_method = 'gmm', 
-                    cluster_channels = ['FL1-H', 'FL2-H', 'FL3-H'],
-                    mef_channels = 'FL1-H', verbose = True, 
+                    cluster_channels = fl_channels,
+                    mef_channels = mef_channels, verbose = True, 
                     plot = True, plot_dir = beads_plot_dir)
 
 
@@ -75,21 +90,21 @@ if __name__ == "__main__":
         di = fc.io.FCSData('{}/{}'.format(directory, df))
         data.append(di)
 
-        gain = di[:,'FL1-H'].channel_info[0]['pmt_voltage']
-        print "{} ({} events, FL1-H gain = {}).".format(str(di), 
+        gain = di[:,'FL1'].channel_info[0]['pmt_voltage']
+        print "{} ({} events, FL1 gain = {}).".format(str(di), 
             di.shape[0], gain)
 
     # Basic gating/trimming
-    ch_all = ['FSC-H', 'SSC-H', 'FL1-H', 'FL2-H', 'FL3-H']
     data = [fc.gate.start_end(di, num_start=250, num_end=100) for di in data]
-    data = [fc.gate.high_low(di, ch_all) for di in data]
+    data = [fc.gate.high_low(di, sc_channels) for di in data]
+    # Gating of fluorescence channels
+    data = [fc.gate.high_low(di, mef_channels) for di in data]
     # Exponential transformation
-    data_transf = [fc.transform.exponentiate(di, ['FSC-H', 'SSC-H']) \
-                                                                for di in data]
+    data_transf = [fc.transform.exponentiate(di, sc_channels) for di in data]
 
-    # Transform to MEFL
-    print "\nTransforming FL1-H to MEFL..."
-    data_transf = [to_mef(di, 'FL1-H') for di in data_transf]
+    # Transform to MEF
+    print "\nTransforming to MEF..."
+    data_transf = [to_mef(di, mef_channels) for di in data_transf]
 
     # Density gate
     print "\nRunning density gate on data files..."
@@ -97,23 +112,33 @@ if __name__ == "__main__":
     data_gated_contour = []
     for di in data_transf:
         print "{}...".format(str(di))
-        di_gated, gate_contour = fc.gate.density2d(data = di, 
-                                            gate_fraction = 0.3)
+        di_gated, gate_contour = fc.gate.density2d(data = di,
+                                            channels = sc_channels,
+                                            gate_fraction = 0.2)
         data_gated.append(di_gated)
         data_gated_contour.append(gate_contour)
 
     # Plot
+    xlabels = [mef_names[chi] for chi in mef_channels]
     if plot_gated:
         print "\nPlotting density diagrams and histograms of data"
         for di, dig, dgc in zip(data_transf, data_gated, data_gated_contour):
             print "{}...".format(str(di))
+            # Create histogram parameters
+            hist_params = []
+            for chi in mef_channels:
+                param = {}
+                param['div'] = 4
+                param['facecolor'] = hist_colors[chi]
+                param['xlabel'] = mef_names[chi]
+                param['log'] = True
+                hist_params.append(param)
             # Plot
             fc.plot.density_and_hist(di, gated_data = dig, figsize = (7,7),
-                density_channels = ['FSC-H', 'SSC-H'], 
-                hist_channels = ['FL1-H'], gate_contour = dgc, 
+                density_channels = sc_channels,
+                hist_channels = mef_channels, gate_contour = dgc, 
                 density_params = {'mode': 'scatter', 'log': True}, 
-                hist_params = {'div': 4, 'log': True, 'edgecolor': 'g',
-                    'xlabel': 'Molecules of Equivalent Fluorescein (MEFL)'},
+                hist_params = hist_params,
                 savefig = '{}/{}.png'.format(gated_plot_dir, str(di)))
             pyplot.close()
             gc.collect()
@@ -122,7 +147,7 @@ if __name__ == "__main__":
     print "\nGenerating bar plot..."
 
     labels = ['Sample {}'.format(i + 1) for i in range(len(data))]
-    fc.plot.hist_and_bar(data_gated, channel = 'FL1-H', labels = labels,
+    fc.plot.hist_and_bar(data_gated, channel = 'FL1', labels = labels,
         hist_params = {'log': True, 'div': 4,
                 'xlabel': 'MEFL', 'ylim': (0, 400)},
         bar_params = {'ylabel': 'MEFL', 'ylim': (0, 40000)},
