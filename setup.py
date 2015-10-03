9e6cee2eb5dbad629905f1389cf969e814f7b875
Sebastian M. Castillo Hair <castillohair@gmail.com>
2015-10-02 20:58:17 -0500

    Requirement for openpyxl is == instead of >=.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
                       'matplotlib>=1.4.3', 
                       'scikit-learn>=0.16.1', 
                       'xlrd>=0.9.3', 
-                      'openpyxl>=2.0.2'],
+                      'openpyxl==2.0.2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
