075a667eee57aa7f9189042fcd6f2c23dd07f240
Sebastian M. Castillo Hair <castillohair@gmail.com>
2015-10-04 21:13:02 -0500

    Increased version number to 0.7.1

--- fc/__init__.py
+++ fc/__init__.py
@@ -1,7 +1,7 @@
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # https://packaging.python.org/en/latest/single_source_version.html
-__version__ = '0.7'
+__version__ = '0.7.1'
 
 import io
 import excel_io
