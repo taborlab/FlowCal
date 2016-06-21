cff3c2016151890e1eb93a91cc8c17fa99bd3876
Sebastian M. Castillo Hair <castillohair@gmail.com>
2016-06-20 23:18:01 -0500

    First iteration of analyze.py.

--- examples/analyze.py
+++ examples/analyze.py
@@ -3,177 +3,215 @@ import os
 import os.path
 
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
-import palettable
 
 import FlowCal
 
-# Directories
-directory = 'FCFiles'
+# Names of the .fcs files to process
+beads_filename = 'FCFiles/Beads006.fcs'
+samples_filenames = ['FCFiles/Data001.fcs',
+                     'FCFiles/Data002.fcs',
+                     'FCFiles/Data003.fcs',
+                     'FCFiles/Data004.fcs',
+                     'FCFiles/Data005.fcs',
+                     ]
+
+# Directories to save plots
 beads_plot_dir = 'plot_beads'
-gated_plot_dir = 'plot_samples'
+samples_plot_dir = 'plot_samples'
 
-# Plot options
-plot_gated = True
-
-# Files
-beads_file = 'data_006.fcs'
-data_files = ['data_{:03d}.fcs'.format(i) for i in range(1,6)]
-
-# Channels
-sc_channels = ['FSC', 'SSC']
-fl_channels = ['FL1', 'FL2', 'FL3']
-# Colors for histograms
-cm = palettable.colorbrewer.diverging.Spectral_8_r.mpl_colormap
-hist_colors_list = [cm(i) for i in np.linspace(0,1,len(fl_channels))]
-hist_colors = dict(zip(fl_channels, hist_colors_list))
-
-# MEF channels
-mef_channels = ['FL1']
-# MEF type used per channel
-mef_names = {'FL1': 'Molecules of Equivalent Fluorescein, MEFL',
-            }
 # MEF bead values
-mef_values = {'FL1': [0, 792, 2079, 6588, 16471, 47497, 137049, 271647],
-             }
+mefl_values = [0, 646, 1704, 4827, 15991, 47609, 135896, 273006]
+
+# IPTG concentration of each sample in micromolar
+iptg = np.array([0, 81, 161, 318, 1000])
 
 if __name__ == "__main__":
-    # Check that directories exists, create if they don't.
+
+    # Check that plot directories exist, create if they don't.
     if not os.path.exists(beads_plot_dir):
         os.makedirs(beads_plot_dir)
-    if not os.path.exists(gated_plot_dir):
-        os.makedirs(gated_plot_dir)
+    if not os.path.exists(samples_plot_dir):
+        os.makedirs(samples_plot_dir)
 
+    ###
+    # Process Beads Files
+    ###
+    print("\nProcessing calibration beads...")
     # Load beads data
-    print("\nProcessing beads...")
-    beads_data = FlowCal.io.FCSData('{}/{}'.format(directory, beads_file))
-    print("Beads file contains {} events.".format(beads_data.shape[0]))
+    print("Loading file \"{}\"...".format(beads_filename))
+    beads_sample = FlowCal.io.FCSData(beads_filename)
 
-    # Transform to Relative Fluorescence Units (rfi), commonly known as
-    # arbitrary units (a.u.)
-    beads_data = FlowCal.transform.to_rfi(beads_data,
-                                          sc_channels + fl_channels)
+    # Transform all channels to Units of Relative Fluorescence Intensity (RFI),
+    # otherwise known as arbitrary units (a.u.)
+    print("Performing data transformation...")
+    beads_sample = FlowCal.transform.to_rfi(beads_sample)
 
     # Gating
+    print("Performing gating...")
 
-    # Remove first and last events. Transients in fluidics can make the
-    # first few and last events slightly different from the rest.
-    beads_data = FlowCal.gate.start_end(beads_data, num_start=250, num_end=100)
+    # Remove first and last events. Transients in fluidics can make the first
+    # few and last events slightly different from the rest.
+    beads_sample_gated = FlowCal.gate.start_end(beads_sample,
+                                                num_start=250,
+                                                num_end=100)
 
     # Remove saturating events in forward/side scatter.
-    beads_data = FlowCal.gate.high_low(beads_data, sc_channels)
+    beads_sample_gated = FlowCal.gate.high_low(beads_sample_gated,
+                                               channels=['FSC','SSC'])
 
-    # Apply density gating
-    print("\nRunning density gate on beads data...")
-    gated_beads_data, __, gate_contour = FlowCal.gate.density2d(
-        data=beads_data,
-        channels=sc_channels,
+    # Density gating
+    beads_sample_gated, __, gate_contour = FlowCal.gate.density2d(
+        data=beads_sample_gated,
+        channels=['FSC','SSC'],
         gate_fraction=0.3,
+        xscale='logicle',
+        yscale='logicle',
+        sigma=5.,
         full_output=True)
 
     # Plot forward/side scatter density plot and fluorescence histograms
-    plt.figure(figsize = (6,4))
+    print("Plotting density plot and histogram...")
+    # Parameters for the forward/side scatter density plot
+    density_params = {}
+    density_params['mode'] = 'scatter'
+    density_params['xscale'] = 'logicle'
+    density_params['yscale'] = 'logicle'
+    density_params['xlim'] = [90, 1023]
+    density_params['ylim'] = [90, 1023]
+    density_params['sigma'] = 5.
+    # Parameters for the fluorescence histograms
+    hist_params = {'xscale': 'logicle'}
+    # Figure name
+    figname = '{}/density_hist_{}.png'.format(beads_plot_dir, 'beads')
+    # Make figure
     FlowCal.plot.density_and_hist(
-        beads_data,
-        gated_beads_data, 
-        density_channels=sc_channels,
-        hist_channels=fl_channels,
+        beads_sample,
+        beads_sample_gated,
+        density_channels=['FSC','SSC'],
+        hist_channels=['FL1', 'FL3'],
         gate_contour=gate_contour, 
-        density_params={'mode': 'scatter', 'xscale': 'log', 'yscale': 'log'},
-        hist_params={'ylim': (0, 1000), 'xscale': 'log'},
-        savefig='{}/density_hist_{}.png'.format(beads_plot_dir, beads_file))
-    plt.close()
+        density_params=density_params,
+        hist_params=hist_params,
+        savefig=figname)
 
     # Obtain standard curve transformation
-    print("\nCalculating standard curve...")
-    peaks_mef = np.array([mef_values[chi] for chi in mef_channels])
-    to_mef = FlowCal.mef.get_transform_fxn(
-        gated_beads_data,
-        peaks_mef,
-        clustering_channels = fl_channels,
-        mef_channels = mef_channels,
-        verbose = True,
-        plot = True,
-        plot_dir = beads_plot_dir)
+    print("\nCalculating standard curve for channel FL1...")
+    mef_transform_fxn = FlowCal.mef.get_transform_fxn(
+        beads_sample_gated,
+        mef_values=mefl_values,
+        mef_channels='FL1',
+        clustering_channels=['FL1', 'FL3'],
+        verbose=True,
+        plot=True,
+        plot_dir=beads_plot_dir,
+        plot_filename='beads')
 
+    ###
+    # Process Cell Sample Files
+    ###
+    print("\nProcessing cell samples...")
 
-    # Load cell sample data
-    print("\nLoading data...")
-    data = []
-    for df in data_files:
-        di = FlowCal.io.FCSData('{}/{}'.format(directory, df))
-        data.append(di)
+    # Initialize samples list
+    samples = []
+    for sample_id, sample_filename in enumerate(samples_filenames):
+        # Load cell sample file
+        print("\nLoading file \"{}\"...".format(sample_filename))
+        sample = FlowCal.io.FCSData(sample_filename)
 
-        dv = di.detector_voltage('FL1')
-        print("{} ({} events, FL1 voltage = {}).".format(
-            str(di),
-            di.shape[0],
-            dv))
+        # Transform
+        print("Performing data transformation...")
 
-    # Transform to Relative Fluorescence Units (rfi), commonly known as
-    # arbitrary units (a.u.)
-    data_transf = [FlowCal.transform.to_rfi(di, sc_channels + mef_channels)
-                   for di in data]
+        # Transform all channels to Units of Relative Fluorescence Intensity
+        # (RFI), otherwise known as arbitrary units (a.u.)
+        sample = FlowCal.transform.to_rfi(sample)
+        # Transform Fluorescence channel FL1 to MEF
+        sample = mef_transform_fxn(sample, channels=['FL1'])
 
-    # Transform specified fluorescence channels to MEF
-    print("\nTransforming to MEF...")
-    data_transf = [to_mef(di, mef_channels) for di in data_transf]
+        # Gating
+        print("Performing gating...")
 
-    # Gate
+        # Remove first and last events. Transients in fluidics can make the
+        # first few and last events slightly different from the rest.
+        sample_gated = FlowCal.gate.start_end(sample,
+                                              num_start=250,
+                                              num_end=100)
 
-    # Remove first and last events. Transients in fluidics can make the
-    # first few and last events slightly different from the rest.
-    data_gated = [FlowCal.gate.start_end(di, num_start=250, num_end=100)
-            for di in data_transf]
+        # Remove saturating events in forward/side scatter and FL1.
+        sample_gated = FlowCal.gate.high_low(sample_gated,
+                                             channels=['FSC','SSC','FL1'])
 
-    # Remove saturating events in forward/side scatter and fluorescence
-    # channels.
-    data_gated = [FlowCal.gate.high_low(di, sc_channels + mef_channels)
-            for di in data_gated]
-
-    # Density gate
-    print("\nRunning density gate on data files...")
-    data_dg = []
-    data_dg_contour = []
-    for di in data_gated:
-        print("{}...".format(str(di)))
-        di_gated, __, gate_contour = FlowCal.gate.density2d(
-            data=di,
-            channels=sc_channels,
-            xscale='log',
-            yscale='log',
-            gate_fraction=0.2,
+        # Density gating
+        sample_gated, __, gate_contour = FlowCal.gate.density2d(
+            data=sample_gated,
+            channels=['FSC','SSC'],
+            gate_fraction=0.5,
+            xscale='logicle',
+            yscale='logicle',
             full_output=True)
-        data_dg.append(di_gated)
-        data_dg_contour.append(gate_contour)
 
-    # Plot
-    xlabels = [mef_names[chi] for chi in mef_channels]
-    if plot_gated:
-        print("\nPlotting density diagrams and histograms of data")
-        for di, dig, dgc in zip(data_transf, data_dg, data_dg_contour):
-            print("{}...".format(str(di)))
-            # Create histogram parameters
-            hist_params = []
-            for chi in mef_channels:
-                param = {}
-                param['facecolor'] = hist_colors[chi]
-                param['xlabel'] = mef_names[chi]
-                param['xscale'] = 'log'
-                hist_params.append(param)
-            # Plot
-            FlowCal.plot.density_and_hist(di,
-                gated_data=dig,
-                figsize=(7,7),
-                density_channels=sc_channels,
-                hist_channels=mef_channels,
-                gate_contour=dgc, 
-                density_params={'mode': 'scatter',
-                                'xscale': 'log',
-                                'yscale': 'log'},
-                hist_params=hist_params,
-                savefig='{}/{}.png'.format(gated_plot_dir, str(di)))
-            plt.close()
+        # Plot forward/side scatter density plot and fluorescence histograms
+        print("Plotting density plot and histogram...")
+        # Parameters for the forward/side scatter density plot
+        density_params = {}
+        density_params['mode'] = 'scatter'
+        density_params['xscale'] = 'logicle'
+        density_params['yscale'] = 'logicle'
+        # Parameters for the fluorescence histograms
+        hist_params = {}
+        hist_params['xscale'] = 'logicle'
+        hist_params['xlabel'] = 'FL1 ' + \
+            '(Molecules of Equivalent Fluorescein, MEFL)'
+        # Figure name
+        figname = '{}/density_hist_{}.png'.format(
+            samples_plot_dir,
+            'S{:03}'.format(sample_id + 1))
+        # Make figure
+        FlowCal.plot.density_and_hist(
+            sample,
+            sample_gated,
+            density_channels=['FSC','SSC'],
+            hist_channels=['FL1'],
+            gate_contour=gate_contour,
+            density_params=density_params,
+            hist_params=hist_params,
+            savefig=figname)
+
+        # Save cell sample object
+        samples.append(sample_gated)
+
+    ###
+    # Plot combined histograms and dose-response curves
+    ###
+
+    # Histogram of all samples
+    plt.figure(figsize=(6,3.5))
+    FlowCal.plot.hist1d(
+        samples,
+        channel=['FL1'],
+        histtype='step',
+        bins=128)
+    plt.ylim([0, 2000])
+    plt.xlabel('FL1  (Molecules of Equivalent Fluorescein, MEFL)')
+    plt.legend(['{} $\mu M$ IPTG'.format(i) for i in iptg],
+               loc='upper left',
+               fontsize='small')
+    plt.tight_layout()
+    plt.savefig('histograms.png', dpi=200)
+    plt.close()
+
+    # Dose response curve
+    samples_fluorescence = [FlowCal.stats.gmean(s, channels='FL1')
+                            for s in samples]
+    plt.figure(figsize=(5.5, 3.5))
+    plt.plot(iptg,
+             samples_fluorescence,
+             marker='o',
+             color=(0, 0.4, 0.7))
+    plt.xlabel('IPTG Concentration ($\mu M$)')
+    plt.ylabel('FL1 Fluorescence (MEFL)')
+    plt.tight_layout()
+    plt.savefig('dose_response.png', dpi=200)
+    plt.close()
 
     print("\nDone.")
