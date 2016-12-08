b734eb5c311a64436bfcc8a6ea2918ddc4cb6e52
John T. Sexton <john.t.sexton@rice.edu>
2016-12-07 22:49:16 -0600

    Added exception for reading linear amplifier gain from FlowJo FCS files.
    See #219.

--- FlowCal/io.py
+++ FlowCal/io.py
@@ -1527,8 +1527,16 @@ class FCSData(np.ndarray):
         detector_voltage = tuple(detector_voltage)
 
         # Amplifier gain: Stored in the keyword parameter $PnG for channel n.
-        amplifier_gain = [fcs_file.text.get('$P{}G'.format(i))
-                          for i in range(1, num_channels + 1)]
+        # The FlowJo Collector's Edition software saves the amplifier gain in
+        # keyword parameters CytekP01G, CytekP02G, CytekP03G, ... for channels
+        # 1, 2, 3, ...
+        if 'CREATOR' in fcs_file.text and \
+                'FlowJoCollectorsEdition' in fcs_file.text.get('CREATOR'):
+            amplifier_gain = [fcs_file.text.get('CytekP{:02d}G'.format(i))
+                              for i in range(1, num_channels + 1)]
+        else:
+            amplifier_gain = [fcs_file.text.get('$P{}G'.format(i))
+                              for i in range(1, num_channels + 1)]
         amplifier_gain = [float(agi) if agi is not None else None
                           for agi in amplifier_gain]
         amplifier_gain = tuple(amplifier_gain)
