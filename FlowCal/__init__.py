f4137472983b701532df12e0e7a6e38a4d355a74
Sebastian Castillo-Hair <castillohair@gmail.com>
2019-10-17 18:21:15 -0500

    Increased version number.

--- FlowCal/__init__.py
+++ FlowCal/__init__.py
@@ -6,7 +6,7 @@
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # https://packaging.python.org/en/latest/single_source_version.html
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 
 from . import io
 from . import excel_ui
