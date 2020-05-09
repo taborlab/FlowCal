6b24a61ebe0ca625e3c8921ba199a39420f6915b
John T. Sexton <sexton.john.t@gmail.com>
2020-05-09 16:07:01 -0500

    Correct scipy minimum requirement.
    `mef.clustering_gmm` uses the `assume_a` parameter of
    `scipy.linalg.solve`, which was not introduced until `scipy`
    version 0.19.0. Make `scipy` v0.19.0 the minimum requirement for
    FlowCal.

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     install_requires=['packaging>=16.8',
                       'six>=1.10.0',
                       'numpy>=1.8.2',
-                      'scipy>=0.14.0',
+                      'scipy>=0.19.0',
                       'matplotlib>=2.0.0',
                       'palettable>=2.1.1',
                       'scikit-image>=0.10.0',
